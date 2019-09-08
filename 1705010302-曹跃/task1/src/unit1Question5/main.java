aaedd846af60c49f49e17fe15a9d8dbdbbd29eb3
yuetsao1024 <41182513+yuetsao1024@users.noreply.github.com>
2019-09-08 23:02:10 +0800

    Add files via upload

---
+++ 1705010302-曹跃/task1/src/unit1Question5/main.java
@@ -0,0 +1,18 @@
+package unit1Question5;
+
+import java.awt.EventQueue;
+
+public  class main {
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					view frame = new view();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+}
